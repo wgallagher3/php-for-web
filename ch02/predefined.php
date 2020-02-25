1a803de463c37fe7cc0f47c42b4b403bac3bd9b6
William Gallagher <wgallagher3@gmail.com>
2020-02-25 11:02:13 -0600

    work on chapter 2

---
+++ ch02/predefined.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Predefined Variables</title>
+</head>
+<body>
+    <pre>
+    <?php //Script 2.1 - predefined.php
+
+    // Show the value of $_SERVER variable:
+    print_r($_SERVER);
+
+    ?>
+    </pre>
+</body>
+</html>
