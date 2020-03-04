e1fda13348fe20fbfbedbe08a4e57fc09e626225
William Gallagher <wgallagher3@gmail.com>
2020-03-04 12:09:56 -0600

    work on chapter 4

---
+++ ch04/random.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lucky Numbers</title>
+</head>
+<body>
+    <?php // Script 4.6 - random-php
+    /* This script generates 3 random numbers. */
+
+    // Address error handling, if you want.
+
+    // Create three random numbers:
+    $n1 = mt_rand(1, 99);
+    $n2 = mt_rand(1, 99);
+    $n3 = mt_rand(1, 99);
+
+    // Print out the numbers:
+    print "<p>Your lucky numbers are:<br>
+    $n1<br>
+    $n2<br>
+    $n3</p>";
+
+
+    ?>
+</body>
+</html>
