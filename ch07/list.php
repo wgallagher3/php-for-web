7a02e3c47b9adee74bdbe102b84351f259f98b1e
William Gallagher <wgallagher3@gmail.com>
2020-03-23 12:45:05 -0500

    all chapter 7

---
+++ ch07/list.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>I Have This Sorted Out</title>
+</head>
+<body>
+    <?php // Script 7.7 - list.php
+    /* This script receives a string in $_POST['words'].
+    It then turns it into an array, sorts the array
+    alphabetically, and reprints it. */
+
+    // Address error management, if you want.
+
+    // Turn the incoming string into an array:
+    $words_array = explode(' ', $_POST['words']);
+
+    // Sort the array:
+    sort($words_array);
+
+    // Turn the array back into a string:
+    $string_words = implode('<br>', $words_array);
+
+    // Print the results:
+    print "<p>An alphabetized version of your list is: <br>$string_words</p>";
+
+    ?>
+</body>
+</html>
---
+++ ch07/sort.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Little Gradebook</title>
+</head>
+<body>
+    <?php // Script 7.5 - sort.php
+    /* This script creates, sorts, and
+    prints out an array. */
+
+    // Address error management, if you want.
+
+    // Create the array:
+    $grades = [
+        'Richard' => 95,
+        'Sherwood' => 82,
+        'Toni' => 98,
+        'Franz' => 87,
+        'Melissa' => 75,
+        'Roddy' => 85
+    ];
+
+    // Print the original array:
+    print '<p>Originally the array looks like this: <br>';
+    foreach ($grades as $student => $grade) {
+        print "$student: $grade<br>\n";
+    }
+    print '</p>';
+
+    // Sort by value in reverse order, then print again:
+    arsort($grades);
+    print '<p>After sorting the array by value using arsort(), the array looks like this: <br>';
+    foreach ($grades as $student => $grade) {
+        print "$student: $grade<br>\n";
+    }
+    print '</p>';
+
+    // Sort by key, then print again:
+    ksort($grades);
+    print '<p>After sorting the array by value using ksort(), the array looks like this: <br>';
+    foreach ($grades as $student => $grade) {
+        print "$student: $grade<br>\n";
+    }
+    print '</p>';
+
+    ?>
+</body>
+</html>
