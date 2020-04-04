5c978f1fa62ae7337795c6c6b76a6e117c035558
William Gallagher <wgallagher3@gmail.com>
2020-04-04 08:42:52 -0500

    chapter 8 work

---
+++ ch08/index.php
@@ -0,0 +1,17 @@
+<?php // Script 8.4 - index.php
+/* This is the home page for this site.
+It uses templates to create the layout. */
+
+// Include the header:
+include('templates/header.html');
+// Leave the PHP section to display lots of HTML:
+?>
+
+<h2>Welcome to a J.D. Salinger Fan Club</h2>
+
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa cupiditate, ab eius dolores ea ut accusantium amet hic error repellat dicta quam sint perferendis, autem odio. Nobis dolorem laborum laboriosam?</p>
+
+<?php // Return to PHP.
+include('templates/footer.html');
+// Include the footer.
+?>
